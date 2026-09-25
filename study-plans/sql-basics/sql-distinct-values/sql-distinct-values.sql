8aa4787f9aabb8932050fa64fadb60fef5bc279a
charantejj <140526300+charanjtej@users.noreply.github.com>
2026-09-25 15:42:06 +0530

    Solve sql-distinct-values

---
+++ study-plans/sql-basics/sql-distinct-values/sql-distinct-values.sql
@@ -0,0 +1,7 @@
+-- Write your SQL query here
+SELECT 
+    customer_name,
+    COUNT(DISTINCT product) AS unique_products
+FROM orders
+GROUP BY customer_name
+ORDER BY unique_products DESC, customer_name
