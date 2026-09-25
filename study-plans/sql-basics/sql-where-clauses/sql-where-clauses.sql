ac8e052ff76c0f64cbfd94fd8f08ee7544a0d3b9
charantejj <140526300+charanjtej@users.noreply.github.com>
2026-09-25 11:01:43 +0530

    Solve sql-where-clauses

--- study-plans/sql-basics/sql-where-clauses/sql-where-clauses.sql
+++ study-plans/sql-basics/sql-where-clauses/sql-where-clauses.sql
@@ -1,11 +1,2 @@
 -- Write your SQL query here
-/*
-SELECT name, salary
-FROM employees
-WHERE department IN ('Engineering', 'Marketing') AND salary > 70000;
-*/
--- Enhanced query: Case Insensitive
-
-SELECT name, salary
-FROM employees
-WHERE LOWER(department) IN ('engineering', 'marketing') AND salary > 70000;
+SELECT name, salary FROM employees WHERE department IN ['Engineering', 'Marketing'] AND salary > 70000;
